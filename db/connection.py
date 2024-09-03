a47676b75b0ab7f2a1c5f04f46291966221147ee
Cesar Sanchez <cs96548@gmail.com>
2024-09-02 22:27:53 -0400

    delete comments

--- db/connection.py
+++ db/connection.py
@@ -10,10 +10,9 @@ import pymongo
 load_dotenv()
 # Función para obtener un cliente de MongoDB asíncrono
 def get_mongo_client():
-    uri = f"mongodb+srv://Aquirre12:Conext123@mongodbtest.w3d21.mongodb.net/?retryWrites=true&w=majority&appName=MongoDBtest"
+    uri = f""
     
     try:
-        # uri = "mongodb://Aquirre12:Conext123@mongodbtest.w3d21.mongodb.net/"
         client = pymongo.MongoClient(uri, server_api=ServerApi('1'))
         return client
     except Exception as e:
